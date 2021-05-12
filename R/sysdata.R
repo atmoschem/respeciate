5ad943cf8e9ce4c914d66009a0bdfd048ad34af4
karlropkins <karl.ropkins@gmail.com>
2021-05-12 16:51:05 +0100

    testing

---
+++ R/sysdata.R
@@ -0,0 +1,17 @@
+############################################
+#' @title sysdata
+############################################
+#'
+#' @name sysdata
+#' @description splat_packed data [to be described]
+#'
+#' @format A ( 5  long) 'list' object
+#' \describe{
+#'   \item{PROFILES}{[to document]}
+#'   \item{SPECIES}{[to document]}
+#'   \item{SPECIES_PROPERTIES}{[to document]}
+#'   \item{PROFILE_REFERENCE}{[to document]}
+#'   \item{REFERENCES}{[to document]}
+#' }
+#' @source [to doc]
+"sysdata"
